6b9340bbb7fac5ff463ac8e48b2ecb0499270271
PraveenSelvamTCE <47314027+PraveenSelvamTCE@users.noreply.github.com>
2020-06-05 13:08:56 +0530

    First push

---
+++ FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Maximum Float Value = " + myMaxFloatValue);
+        System.out.println("Minimum Float Value = " + myMinFloatValue);
+        System.out.println("Maximum Double Value = " + myMaxDoubleValue);
+        System.out.println("Minimum Double Value = " + myMinDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+
+        System.out.println("Integer Value = " + myIntValue);
+        System.out.println("Float Value = " + myFloatValue);
+        System.out.println("Double Value = " + myDoubleValue);
+    }
+}
